6d7e87c9c33ee7333deeffe9f0cabdd1f3b8aea8
Stelchenko <59118095+Stelchenko@users.noreply.github.com>
2020-04-19 19:40:53 +0300

    Add files via upload

---
+++ build/default/main/levels/level3_generated_0.go
@@ -0,0 +1,46 @@
+components {
+  id: "board"
+  component: "/main/scripts/board.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "level"
+  component: "/main/guis/level.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "block_factory"
+  type: "factory"
+  data: "prototype: \"/main/block.go\"\nload_dynamically: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ build/default/main/main_generated_1.go
@@ -0,0 +1,95 @@
+components {
+  id: "loader"
+  component: "/main/scripts/loader.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "proxy_level_1"
+  type: "collectionproxy"
+  data: "collection: \"/main/levels/level1.collection\"\nexclude: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "proxy_level_2"
+  type: "collectionproxy"
+  data: "collection: \"/main/levels/level2.collection\"\nexclude: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "proxy_level_3"
+  type: "collectionproxy"
+  data: "collection: \"/main/levels/level3.collection\"\nexclude: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "proxy_level_4"
+  type: "collectionproxy"
+  data: "collection: \"/main/levels/level4.collection\"\nexclude: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "proxy_level_5"
+  type: "collectionproxy"
+  data: "collection: \"/main/levels/level5.collection\"\nexclude: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
